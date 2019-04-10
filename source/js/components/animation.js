ae219ac14a4d465a6139f88af5412777042a9dc5
Your Name <felipe.ferreira.ads@gmail.com>
2019-04-10 14:20:49 -0300

    menu animations

--- source/js/components/animation.js
+++ source/js/components/animation.js
@@ -18,3 +18,29 @@ const numbers = $('.stats').waypoint(direction => {
 }, {
     offset: '80%'
 })
+
+$(window).scroll(() => {
+    if($(window).scrollTop() > 0) {
+        if(!$('header').hasClass('scrolled')) {
+            $('header .logo img').attr('src', 'assets/img/logo-blue.svg')
+            
+            $('header').animate({
+                height: 70
+            }, 200).css({
+                background: '#fff',
+                boxShadow: '0 0 3px rgba(0,0,0,0.2)'
+            }).addClass('scrolled')
+        }
+    } else {
+        if($('header').hasClass('scrolled')) {
+            $('header .logo img').attr('src', 'assets/img/logo.svg')
+
+            $('header').animate({
+                height: 100
+            }, 200).css({
+                background: 'transparent',
+                boxShadow: 'none'
+            }).removeClass('scrolled')
+        }
+    }
+})
